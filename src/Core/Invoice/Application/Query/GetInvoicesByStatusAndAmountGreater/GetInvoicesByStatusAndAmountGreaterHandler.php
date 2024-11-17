0f007e908d3a3913e86317b48cf024a1650bcc14
Ceglaa02 <jcplewiska@interia.pl>
2024-11-17 15:03:23 +0100

    fixed bug included with app:invoice:get-by-status-and-amount cli command, now this returns correct result by params status and amount

--- src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
+++ src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
@@ -3,6 +3,7 @@
 namespace App\Core\Invoice\Application\Query\GetInvoicesByStatusAndAmountGreater;
 
 use App\Core\Invoice\Application\DTO\InvoiceDTO;
+use App\Core\Invoice\Domain\Exception\InvoiceException;
 use App\Core\Invoice\Domain\Invoice;
 use App\Core\Invoice\Domain\Repository\InvoiceRepositoryInterface;
 use App\Core\Invoice\Domain\Status\InvoiceStatus;
@@ -17,9 +18,15 @@ class GetInvoicesByStatusAndAmountGreaterHandler
 
     public function __invoke(GetInvoicesByStatusAndAmountGreaterQuery $query): array
     {
+        $status = InvoiceStatus::tryFrom($query->status);
+
+        if(is_null($status)) {
+            throw new InvoiceException('Błąd, podano niepoprawny status');
+        }
+
         $invoices = $this->invoiceRepository->getInvoicesWithGreaterAmountAndStatus(
             $query->amount,
-            InvoiceStatus::CANCELED
+            $status,
         );
 
         return array_map(function (Invoice $invoice) {
